8d57e6eb2df425249958ba7461ef78f1bc4f7e49
Amir Soveizi <amirsoveizi2004@gmail.com>
2025-05-14 13:55:13 +0330

    add linq controller

---
+++ ProNews/Content/Linq/js/actions.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    function updateContainer(url) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (html) {
+                $('#Container').html(html);
+            },
+            error: function () {
+                alert("Failed to load names.");
+            }
+        });
+    }
+
+    $('#sort-desc').click(() => updateContainer('/Linq/SortDescending'));
+    $('#show-names-s').click(() => updateContainer('/Linq/ShowNamesWithS'));
+    $('#last-3-names').click(() => updateContainer('/Linq/Last3Names'));
+    $('#first-last-names').click(() => updateContainer('/Linq/FirstAndLast'));
+});
---
+++ ProNews/Controllers/LinqController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProNews.Controllers
+{
+    public class LinqController : Controller
+    {
+        List<string> names = new List<string> { "John", "Samuel", "Liam", "Olivia", "Noah", "Ava", "Sam", "Sophia", "James", "Isabella" };
+        // GET: Linq
+        public ActionResult Index()
+        {
+            return View(names);
+        }
+
+        public ActionResult SortDescending()
+        {
+            var sortedNames = (from name in names
+                               orderby name descending
+                               select name).ToList();
+            return PartialView("_List",sortedNames);
+        }
+
+        public ActionResult ShowNamesWithS()
+        {
+            var namesWithS = names.Where(name => name.StartsWith("S", StringComparison.OrdinalIgnoreCase) && name.Length > 3).ToList();
+
+            return PartialView("_List", namesWithS);
+        }
+
+        public ActionResult Last3Names()
+        {
+            var last3Names = (from name in names
+                              let index = names.IndexOf(name)
+                              where index >= names.Count - 3
+                              select name
+                              ).ToList();
+
+            return PartialView("_List", last3Names);
+        }
+
+        public ActionResult FirstAndLast()
+        {
+            var firstAndLast = (from name in names
+                                let index = names.IndexOf(name)
+                                where index == 0 || index == names.Count - 1
+                                select name
+                                ).ToList();
+
+
+            return PartialView("_List", firstAndLast);
+        }
+    }
+}
